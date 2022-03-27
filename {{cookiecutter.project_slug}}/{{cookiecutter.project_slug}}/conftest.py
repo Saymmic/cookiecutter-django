290ab318c25abb70ed802de4203e27c6d2fda1a8
Michal Simonienko <michal.simonienko@climatica.ai>
2022-03-27 13:44:49 +0200

    Add api_client fixtures

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/conftest.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from rest_framework.test import APIClient
 
 from {{ cookiecutter.project_slug }}.users.models import User
 from {{ cookiecutter.project_slug }}.users.tests.factories import UserFactory
@@ -12,3 +13,21 @@ def media_storage(settings, tmpdir):
 @pytest.fixture
 def user() -> User:
     return UserFactory()
+
+
+@pytest.fixture
+def test_user() -> User:
+
+    return UserFactory.create(
+        username="APITestUser",
+        email="api_test_user@gmail.com",
+        name="API Test User",
+    )
+
+
+@pytest.fixture
+def api_client(test_user) -> APIClient:
+    api_client = APIClient()
+    api_client.force_authenticate(user=test_user)
+
+    return api_client
